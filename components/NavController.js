3e42192096e4cdb7f53785f2562ba81fcde90482
Sangin <sangin.kang@navercorp.com>
2019-10-19 07:35:24 +0900

    #11.2 Tabs Navigation

---
+++ components/NavController.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
+import AuthNavigation from "../navigation/AuthNavigation";
+import TabNavigation from "../navigation/TabNavigation";
+
+export default () => {
+  const isLoggedIn = useIsLoggedIn();
+  const logIn = useLogIn();
+  const logOut = useLogOut();
+
+  return (
+    <View style={{ flex: "1" }}>
+      {isLoggedIn ? <TabNavigation /> : <AuthNavigation />}
+    </View>
+  );
+};
